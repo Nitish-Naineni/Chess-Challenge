90ea7d90a170ff897c473bad4b5152022158eb80
Nitish-Naineni <96668765+Nitish-Naineni@users.noreply.github.com>
2023-08-10 10:12:41 +0530

    reformated search function

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,8 +6,8 @@ public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     readonly int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    readonly TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
-    Dictionary<ulong, Tuple<int, int, Move>> trans = new();
+    readonly TimeSpan timeout = TimeSpan.FromMilliseconds(500);
+    Dictionary<ulong, (int, int, Move)> trans = new();
     Dictionary<ulong, Move[]> moveHistroy;
     DateTime startTime;
 
@@ -44,73 +44,46 @@ public class MyBot : IChessBot
         return totalValue;
     }
 
-    Tuple<int,Move> Search (Board board, int depth, int alpha, int beta){
+    (int,Move) Search (Board board, int depth, int alpha, int beta){
         if (DateTime.UtcNow - startTime > timeout){throw new Exception("Search Halted");}
 
         ulong zKey = board.ZobristKey;
-        if (trans.ContainsKey(zKey) && trans[zKey].Item2 >= depth){
-            return new Tuple<int, Move>(trans[zKey].Item1, trans[zKey].Item3);
-        }
+        if (trans.ContainsKey(zKey) && trans[zKey].Item2 >= depth){return (trans[zKey].Item1, trans[zKey].Item3);}
+        if (depth == 0){return (Quiesce(board, alpha, beta), Move.NullMove);}
 
-        if (depth == 0){
-            return new Tuple<int, Move>(Quiesce(board, alpha, beta), Move.NullMove);
-        }
+        Move[] moves = moveHistroy.ContainsKey(zKey) ? moveHistroy[zKey] : OrderMoves(board.GetLegalMoves());
+        if (moves.Length == 0 || board.IsDraw()){return (0, Move.NullMove);}
 
-        Move[] moves;
-        if (moveHistroy.ContainsKey(zKey)){
-            moves = moveHistroy[zKey];
-        }else{
-            moves = OrderMoves(board.GetLegalMoves());
-        }
         int[] moveScores = new int[moves.Length];
         for (int i = 0; i < moveScores.Length; i++){moveScores[i] = int.MinValue;}
-
-        if (moves.Length == 0){
-            return new Tuple<int, Move>(0, Move.NullMove);
-        }
 
         Random rng = new();
         Move bestMove = moves[rng.Next(moves.Length)];
 
-        int score;
         for (int i = 0; i < moves.Length; i++){
             board.MakeMove(moves[i]);
-            if (board.IsInCheckmate()){
-                score = pieceValues[6];
-            }else if (board.IsDraw()){
-                score = 0;
-            }else{
-                score = -Search(board, depth - 1, -beta, -alpha).Item1;
-            }
-
+            int score = board.IsInCheckmate() ? pieceValues[6] : -Search(board, depth - 1, -beta, -alpha).Item1;
             moveScores[i] = score;
-            
             board.UndoMove(moves[i]);
-            if (score >= beta){
-                return new Tuple<int, Move>(beta,Move.NullMove);
-            }
 
-            if (score > alpha){
-                alpha = score;
-                bestMove = moves[i];
-            }
+            if (score >= beta){return (beta,Move.NullMove);}
+            if (score > alpha){(alpha, bestMove) = (score, moves[i]);}
         }
         Array.Sort(moveScores, moves);
         Array.Reverse(moves);
         moveHistroy[zKey] = moves;
 
-        trans[zKey] = new Tuple<int, int, Move>(alpha, depth, bestMove);
-        return new Tuple<int, Move>(alpha, bestMove);
+        trans[zKey] = (alpha, depth, bestMove);
+        return (alpha, bestMove);
     }
 
 
     int Quiesce(Board board, int alpha, int beta) {
         if (DateTime.UtcNow - startTime > timeout){throw new Exception("Search Halted");}
+
         int stand_pat = Evaluate(board);
-        if (stand_pat >= beta)
-            return beta;
-        if (alpha < stand_pat)
-            alpha = stand_pat;
+        if (stand_pat >= beta){return beta;}
+        if (alpha < stand_pat){alpha = stand_pat;}
 
         Move[] captures = board.GetLegalMoves(true);
         foreach (Move capture in captures) {
@@ -118,23 +91,16 @@ public class MyBot : IChessBot
             int score = -Quiesce(board, -beta, -alpha);
             board.UndoMove(capture);
 
-            if (score >= beta)
-                return beta;
-            if (score > alpha)
-            alpha = score;
+            if (score >= beta){return beta;}
+            if (score > alpha){alpha = score;}
         }
         return alpha;
     }
 
     Move[] OrderMoves(Move[] moves){
         int[] moveScores = new int[moves.Length];
-        Random rng = new();
-        int s = rng.Next(moves.Length);
-        (moves[s], moves[0]) = (moves[0], moves[s]);
         for (int i = 0; i < moves.Length; i++){
-            if (moves[i].IsCapture){
-                moveScores[i] = pieceValues[(int)moves[i].CapturePieceType] - pieceValues[(int)moves[i].MovePieceType];
-            }
+            if (moves[i].IsCapture){moveScores[i] = pieceValues[(int)moves[i].CapturePieceType] - pieceValues[(int)moves[i].MovePieceType];}
         }
         Array.Sort(moveScores, moves);
         Array.Reverse(moves);
